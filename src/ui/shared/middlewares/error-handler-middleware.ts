905afa06841aee28e82181a298db7e3de728b967
Cheryl Soberanes <cherylsoberaness@gmail.com>
2026-07-04 14:34:36 -0600

    refactor: improve error hadler middleware

--- src/ui/shared/middlewares/error-handler-middleware.ts
+++ src/ui/shared/middlewares/error-handler-middleware.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response, } from "express";
 import { ZodError } from "zod";
 import { BusinessConflictError } from "../../../domain/errors/BusinessConflictError";
+import { EntityNotFoundError } from "../../../domain/errors/EntityNotFoundError";
+import { UnauthorizedError } from "../../../domain/errors/UnauthorizedError";
 
 
 export const errorHandlerMiddleware = (
@@ -13,6 +15,10 @@ export const errorHandlerMiddleware = (
         return res.status(400).json({ error: error.issues[0].message });
     } else if (error instanceof BusinessConflictError) {
         return res.status(409).json({ error: error.message });
+    } else if (error instanceof EntityNotFoundError) {
+        return res.status(404).json({ error: error.message });
+    } else if (error instanceof UnauthorizedError){
+        return res.status(401).json({ error: error.message });
     } else {
         return res.status(500).json({ error: 'Internal server error'});
     }
